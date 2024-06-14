9177aee929098bab12aad1e19e1509bc5475649a
akkminn <115207574+akkminn@users.noreply.github.com>
2024-06-14 04:28:14 +0000

    Add watchlist and related functions

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("listing/<int:pk>", views.DeatailsView.as_view(), name="listing")
+    path("listing/<int:pk>", views.DeatailsView.as_view(), name="listing"),
+    path("watchlists/", views.WatchListView.as_view(), name="watchlists"),
+    path("watchlists/add/<int:list_id>/", views.add_to_watchlist, name="add"),
+    path("wathclists/remove/<int:list_id>/", views.remove_from_watchlist, name="remove")
 ]
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,9 +1,11 @@
+from typing import Any
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 
 from .models import User, AuctionList, Bid, Comment, Category
@@ -23,6 +25,26 @@ class DeatailsView(generic.DetailView):
     def get_queryset(self):
         return super().get_queryset()
     
+class WatchListView(generic.ListView):
+    model = AuctionList
+    template_name = "auctions/watchlists.html"
+    context_object_name = "watchlist"
+
+    def get_queryset(self):
+        return self.request.user.watchlist.all()
+
+@login_required
+def add_to_watchlist(request, list_id):
+    auction = get_object_or_404(AuctionList, id=list_id)
+    request.user.watchlist.add(auction)
+    return HttpResponseRedirect(reverse("listing", args=(list_id, )))
+
+@login_required
+def remove_from_watchlist(request, list_id):
+    auction = get_object_or_404(AuctionList, id=list_id)
+    request.user.watchlist.remove(auction)
+    return HttpResponseRedirect(reverse("listing", args=(list_id, )))
+
 
 
 def login_view(request):
