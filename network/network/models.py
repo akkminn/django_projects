e0ba4240d9e128ca776ef4f0b21e7b125b15500c
akkminn <115207574+akkminn@users.noreply.github.com>
2024-06-19 04:31:59 +0000

    Change the UI of navbar

--- network/network/models.py
+++ network/network/models.py
@@ -11,6 +11,21 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True, null=False)
 
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.timestamp}"
+
 class Profile(models.Model):
     follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followings")
     following = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    content = models.TextField(blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.id} at {self.timestamp}"
